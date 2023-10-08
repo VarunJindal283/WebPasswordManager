093d9e6c86255bf1f927e22ba788f11f9643297a
Varun <varun2394.be21@chitkara.edu.in>
2023-10-08 18:27:18 +0530

    completing it till delete and copy method

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 // Logic to fill the table
-let tb=document.querySelector("table");
-let data = localStorage.getItem("passwords");
-if(data == null){
-    tb.innerHTML= "No Data To Show"
-}
-else{
-    let arr=JSON.parse(data);
-    for (let index =0; index<arr.length;index++){
-        const element = arr[index];
-        str =`<tr>
-        <td>${element.website}</td>
-        <td>${element.username}</td>
-        <td>${element.password}</td>
-        <td>${"Delete"}</td>
-        </tr>`
-        tb.innerHTML=tb.innerHTML + str;
+const showPasswords=()=>{ 
+    let tb=document.querySelector("table");
+    let data = localStorage.getItem("passwords");
+    if(data == null){
+        tb.innerHTML= "No Data To Show"
+    }
+    else{
+        tb.innerHTML=`<tr>
+                        <th>Website</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Delete</th>
+                      </tr>`
+        let arr=JSON.parse(data);
+        for (let index =0; index<arr.length;index++){
+            const element = arr[index];
+            str =`<tr>
+            <td>${element.website}</td>
+            <td>${element.username}</td>
+            <td>${element.password}</td>
+            <td>${"Delete"}</td>
+            </tr>`
+            tb.innerHTML=tb.innerHTML + str;
+        }
     }
 }
 
 
 
 console.log("Working");
+showPasswords();
 document.querySelector(".btn").addEventListener("click",(e)=>{
     e.preventDefault() //preventing page to reload or we can say prevent form to get submited
     console.log("clicked");
@@ -29,14 +38,15 @@ document.querySelector(".btn").addEventListener("click",(e)=>{
     console.log(passwords)
     if(passwords==null){
         let json=[]
-        json.push({username:username.value, password:password.value});
+        json.push({website:website.value, username:username.value, password:password.value});
         alert("Password Saved");
         localStorage.setItem("passwords", JSON.stringify(json))
     }
     else{
         let json= JSON.parse(localStorage.getItem("passwords"))
-        json.push({username:username.value, password:password.value});
+        json.push({website:website.value, username:username.value, password:password.value});
         alert("Password Saved");
         localStorage.setItem("passwords", JSON.stringify(json))
     }
+    showPasswords();
 })
