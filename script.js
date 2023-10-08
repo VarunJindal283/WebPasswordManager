0a3fe234b8cc8235112657b08cecc9be2d64725f
Varun <varun2394.be21@chitkara.edu.in>
2023-10-08 18:17:00 +0530

    adding data to table dynamically

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Logic to fill the table
+let tb=document.querySelector("table");
+let data = localStorage.getItem("passwords");
+if(data == null){
+    tb.innerHTML= "No Data To Show"
+}
+else{
+    let arr=JSON.parse(data);
+    for (let index =0; index<arr.length;index++){
+        const element = arr[index];
+        str =`<tr>
+        <td>${element.website}</td>
+        <td>${element.username}</td>
+        <td>${element.password}</td>
+        <td>${"Delete"}</td>
+        </tr>`
+        tb.innerHTML=tb.innerHTML + str;
+    }
+}
+
+
+
 console.log("Working");
 document.querySelector(".btn").addEventListener("click",(e)=>{
     e.preventDefault() //preventing page to reload or we can say prevent form to get submited
