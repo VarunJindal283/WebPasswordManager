2995450018913e8adf6a7ec0ac2b8a90cb981c91
Varun <varun2394.be21@chitkara.edu.in>
2023-10-08 18:59:51 +0530

    adding delete button to passmanger

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // Logic to fill the table
+const deletePassword = (website) =>{
+    let data = localStorage.getItem("passwords");
+    let arr= JSON.parse(data);
+    arrUpdated = arr.filter((e)=>{
+        return e.website !=website
+    });
+    localStorage.setItem("passwords", JSON.stringify(arrUpdated));
+}
+
+
 const showPasswords=()=>{ 
     let tb=document.querySelector("table");
     let data = localStorage.getItem("passwords");
@@ -19,7 +29,7 @@ const showPasswords=()=>{
             <td>${element.website}</td>
             <td>${element.username}</td>
             <td>${element.password}</td>
-            <td>${"Delete"}</td>
+            <td><button class="btnsm" id="${element.website}">Delete</button></td>
             </tr>`
             tb.innerHTML=tb.innerHTML + str;
         }
