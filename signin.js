609232fb3bb52e3704a0e2d4dbe224ecde706769
Varun <varun2394.be21@chitkara.edu.in>
2023-10-28 17:13:40 +0530

    signin page js added

---
+++ signin.js
@@ -0,0 +1,21 @@
+console.log("working");
+document.querySelector("#sub").addEventListener("click",(e)=>{
+    e.preventDefault();
+    console.log("clicked");
+    let users=JSON.parse(localStorage.getItem("users"));
+    let userexist=false;
+    users.forEach((element)=>{
+        if(username.value==element.user && pass.value==element.pass){
+            console.log("entered")
+            userexist=true;
+            window.location.href="index.html";
+        }
+        else if(username.value==element.user && pass.value!=element.pass){
+            userexist=true;
+            console.log("You entered wrong password");
+        }
+    })
+    if(userexist==false){
+        console.log("User not found");
+    }
+})
