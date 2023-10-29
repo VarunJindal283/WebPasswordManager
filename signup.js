edd5b40e2ee1ea03946af0eff590357e323f33b7
Varun <varun2394.be21@chitkara.edu.in>
2023-10-29 15:13:00 +0530

    adding js to modal

--- signup.js
+++ signup.js
@@ -1,13 +1,17 @@
 console.log("heloo");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+var modalText=document.getElementsByClassName("modalText")[0];
 document.querySelector("#sub").addEventListener("click",(e)=>{
-    e.preventDefault();
+    // e.preventDefault();
     console.log("clicked");
     let users=localStorage.getItem("users");
     console.log(username.value,pass.value);
     if(users==null){
         let json=[]
         json.push({user:username.value,pass:pass.value});
-        alert("Account Created");
+        modalText.innerHTML="Account Created";
+        modal.style.display = "block";
         localStorage.setItem("users",JSON.stringify(json));
     }
     else{
@@ -19,15 +23,27 @@ document.querySelector("#sub").addEventListener("click",(e)=>{
             }
         })
         if(userExist){
-            console.log("Please Sign in User already exist");
+            modalText.innerHTML="User already exist.. Please Sign In.";
+            modal.style.display = "block";
+            // console.log("Please Sign in User already exist");
         }
         else{
             json.push({user:username.value,pass:pass.value});
-            alert("Account Created");
+            modalText.innerHTML="Account Created";
+            modal.style.display = "block";
             localStorage.setItem("users",JSON.stringify(json));
         }
     }
+    username.value="";
+    pass.value="";
 });
 document.querySelector(".redirect").addEventListener("click",(e)=>{
     window.location.href="signin.html";
 })
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
