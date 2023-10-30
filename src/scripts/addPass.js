03a6dc5ea5309f0f47a898c7f445c0f2d082158e
Varun <varun2394.be21@chitkara.edu.in>
2023-10-30 17:54:42 +0530

    adding some authenticity

--- src/scripts/addPass.js
+++ src/scripts/addPass.js
@@ -1,56 +1,69 @@
-let PmUser=sessionStorage.getItem("PmUser");
+if(sessionStorage.length!=0){
 
-function checkRedentant(website, username){
-    let arr= JSON.parse(localStorage.getItem(`${PmUser}`)); 
-    let r=false; 
-    arr.forEach((x)=>{
-        if(x.website==website){
-            if(x.username==username){
-                r=true;
+    document.querySelector(".logo").addEventListener("click", (e)=>{
+        window.location.href="home.html"
+    })
+
+    document.querySelector(".logout").addEventListener("click",(e)=>{
+        window.location.href="signin.html";
+        sessionStorage.clear();
+    })
+    
+    
+    
+    let PmUser=sessionStorage.getItem("PmUser");
+    
+    function checkRedentant(website, username){
+        let arr= JSON.parse(localStorage.getItem(`${PmUser}`)); 
+        let r=false; 
+        arr.forEach((x)=>{
+            if(x.website==website){
+                if(x.username==username){
+                    r=true;
+                }
             }
-        }
-    }); 
-    return r;
-}
-
-
-
-console.log("Working");
-document.querySelector(".btnn").addEventListener("click",(e)=>{
-    e.preventDefault() //preventing page to reload or we can say prevent form to get submited
-    console.log("clicked");
-    console.log(username.value, password.value);
-    let passwords=localStorage.getItem(`${PmUser}`)
-    console.log(passwords)
-    if(website.value!="" && username.value!="" && password.value!=""){
-        if(passwords==null){
-            let json=[]
-            json.push({website:website.value, username:username.value, password:password.value});
-            alert("Password Saved");
-            localStorage.setItem(`${PmUser}`, JSON.stringify(json))
-        }
-        
-        else{
-            let json;
-            if(checkRedentant(website.value, username.value)){
-                if(confirm("Want to Update the Password?")){
-                    json= JSON.parse(localStorage.getItem(`${PmUser}`));
-                    json.forEach((x)=>{
-                if(x.website==website.value){
-                    if(x.username==username.value){
-                        x.password=password.value;
+        }); 
+        return r;
+    }
+    
+    
+    
+    console.log("Working");
+    document.querySelector(".btnn").addEventListener("click",(e)=>{
+        e.preventDefault() //preventing page to reload or we can say prevent form to get submited
+        console.log("clicked");
+        console.log(username.value, password.value);
+        let passwords=localStorage.getItem(`${PmUser}`)
+        console.log(passwords)
+        if(website.value!="" && username.value!="" && password.value!=""){
+            if(passwords==null){
+                let json=[]
+                json.push({website:website.value, username:username.value, password:password.value});
+                alert("Password Saved");
+                localStorage.setItem(`${PmUser}`, JSON.stringify(json))
+            }
+            
+            else{
+                let json;
+                if(checkRedentant(website.value, username.value)){
+                    if(confirm("Want to Update the Password?")){
+                        json= JSON.parse(localStorage.getItem(`${PmUser}`));
+                        json.forEach((x)=>{
+                            if(x.website==website.value){
+                                if(x.username==username.value){
+                                    x.password=password.value;
+                                }
+                            }
+                        }); 
+                        localStorage.setItem(`${PmUser}`, JSON.stringify(json))
+                        alert("Password Updated");
+                    }
+                    else{
+                        website.value="";
+                        username.value="";
+                        password.value="";
                     }
                 }
-            }); 
-            localStorage.setItem(`${PmUser}`, JSON.stringify(json))
-            alert("Password Updated");
-        }
-        else{
-            website.value="";
-            username.value="";
-            password.value="";
-        }
-    }
     else{
         json= JSON.parse(localStorage.getItem(`${PmUser}`));
         json.push({website:website.value, username:username.value, password:password.value});
@@ -63,11 +76,15 @@ else{
     modalText.innerHTML="Enter a valid Website, Username and Password";
     modal.style.display = "block";
 }
-    website.value="";
-    username.value="";
-    password.value="";
+website.value="";
+username.value="";
+password.value="";
 })
 
 document.querySelector(".redirect").addEventListener("click",(e)=>{
     window.location.href="home.html";
 })
+}
+else{
+    window.location.href="signin.html";
+}
--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,84 +1,96 @@
-let PmUser=sessionStorage.getItem("PmUser");
+if(sessionStorage.length!=0){
+    document.querySelector(".logo").addEventListener("click", (e)=>{
+        window.location.href="home.html"
+    })
 
-function maskPassword(pass){
-    let str="";
-    for(let index =0; index<pass.length; index++){
-        str += "*";
-    }
-    return str;
-}
-
-
-function copyText(txt) {
-    navigator.clipboard.writeText(txt);
-    // alert("Copied the text: " + txt);
-    // document.querySelector(".alert").classList.remove("alert");
-    document.getElementById("alert").style.display = "inline";
-    setTimeout(()=>{
-        document.getElementById("alert").style.display = "none";
-    },500);
-}
+    document.querySelector(".logout").addEventListener("click",(e)=>{
+        window.location.href="signin.html";
+        sessionStorage.clear();
+    })
 
 
-function deletePassword(website,username){
-    let data = localStorage.getItem(`${PmUser}`);
-    let arr=JSON.parse(data);
-    let arrUpdated=[];
-    arr.forEach((e)=>{
-        if(e.website!=website){
-            arrUpdated.push(e);
+
+    let PmUser=sessionStorage.getItem("PmUser");
+
+    function maskPassword(pass){
+        let str="";
+        for(let index =0; index<pass.length; index++){
+            str += "*";
         }
-        else if(e.website==website){
-            if(e.username!=username){
+        return str;
+    }
+
+
+    function copyText(txt) {
+        navigator.clipboard.writeText(txt);
+        document.getElementById("alert").style.display = "inline";
+        setTimeout(()=>{
+            document.getElementById("alert").style.display = "none";
+        },500);
+    }
+
+
+    function deletePassword(website,username){
+        let data = localStorage.getItem(`${PmUser}`);
+        let arr=JSON.parse(data);
+        let arrUpdated=[];
+        arr.forEach((e)=>{
+            if(e.website!=website){
                 arrUpdated.push(e);
             }
-        }
-    });
-    localStorage.setItem(`${PmUser}`,JSON.stringify(arrUpdated));
-    alert(`Successfully deleted ${website}'s password`);
-}
-
-// Logic to fill the table
-
-const showPasswords=()=>{ 
-    let tb=document.querySelector("table");
-    let data = localStorage.getItem(`${PmUser}`);
-    if(data == null || JSON.parse(data).length == 0){
-        tb.innerHTML= "...."+"No Data To Show "+"...."
+            else if(e.website==website){
+                if(e.username!=username){
+                    arrUpdated.push(e);
+                }
+            }
+        });
+        localStorage.setItem(`${PmUser}`,JSON.stringify(arrUpdated));
+        alert(`Successfully deleted ${website}'s password`);
     }
-    else{
-        tb.innerHTML=`<tr>
-                        <th>Website</th>
-                        <th>Username</th>
-                        <th>Password</th>
-                        <th>Delete</th>
-                      </tr>`
-        let arr=JSON.parse(data);
-        for (let index =0; index<arr.length;index++){
-            const element = arr[index];
-            str =`<tr>
-            <td>${element.website} <img onclick="copyText('${element.website}')" src="../images/copy.svg" alt="Copy Button" width="10" height="10"></td>
-            <td>${element.username} <img onclick="copyText('${element.username}')" src="../images/copy.svg" alt="Copy Button" width="10" height="10"></td>
-            <td>${maskPassword(element.password)} <img onclick="copyText('${element.password}')" src="../images/copy.svg" alt="Copy Button" width="10" height="10"></td>
-            <td><button class="btnsm" onclick="deletePassword('${element.website}','${element.username}')+showPasswords()">Delete</button></td>
-            </tr>`
-            tb.innerHTML=tb.innerHTML + str;
+
+    // Logic to fill the table
+
+    const showPasswords=()=>{ 
+        let tb=document.querySelector("table");
+        let data = localStorage.getItem(`${PmUser}`);
+        if(data == null || JSON.parse(data).length == 0){
+            tb.innerHTML= "...."+"No Data To Show "+"...."
         }
-        website.value="";
-        username.value="";
-        password.value="";
+        else{
+            tb.innerHTML=`<tr>
+                            <th>Website</th>
+                            <th>Username</th>
+                            <th>Password</th>
+                            <th>Delete</th>
+                        </tr>`
+            let arr=JSON.parse(data);
+            for (let index =0; index<arr.length;index++){
+                const element = arr[index];
+                str =`<tr>
+                <td>${element.website} <img onclick="copyText('${element.website}')" src="../images/copy.svg" alt="Copy Button" width="10" height="10"></td>
+                <td>${element.username} <img onclick="copyText('${element.username}')" src="../images/copy.svg" alt="Copy Button" width="10" height="10"></td>
+                <td>${maskPassword(element.password)} <img onclick="copyText('${element.password}')" src="../images/copy.svg" alt="Copy Button" width="10" height="10"></td>
+                <td><button class="btnsm" onclick="deletePassword('${element.website}','${element.username}')+showPasswords()">Delete</button></td>
+                </tr>`
+                tb.innerHTML=tb.innerHTML + str;
+            }
+            website.value="";
+            username.value="";
+            password.value="";
+        }
     }
+
+
+
+    console.log("Working");
+
+    document.querySelector(".redirect").addEventListener("click",(e)=>{
+        window.location.href="addPass.html";
+    })
+
+
+    showPasswords();
 }
-
-
-
-console.log("Working");
-
-document.querySelector(".redirect").addEventListener("click",(e)=>{
-    window.location.href="addPass.html";
-})
-
-
-showPasswords();
-
-
+else{
+    window.location.href="signin.html";
+}
