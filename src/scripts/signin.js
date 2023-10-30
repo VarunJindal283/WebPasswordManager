f3a32eb751fe4648a7d319107ad953ce4a2c2bd9
Varun <varun2394.be21@chitkara.edu.in>
2023-10-30 14:21:14 +0530

    fixed minor bug in signin page

--- src/scripts/signin.js
+++ src/scripts/signin.js
@@ -6,23 +6,30 @@ document.querySelector("#sub").addEventListener("click",(e)=>{
     console.log("clicked");
     let users=JSON.parse(localStorage.getItem("users"));
     let userexist=false;
-    users.forEach((element)=>{
-        if(username.value==element.user && pass.value==element.pass){
-            console.log("entered")
-            userexist=true;
-            window.location.href="home.html";
-        }
-        else if(username.value==element.user && pass.value!=element.pass){
-            userexist=true;
-            modalText.innerHTML="Entered wrong password";
-            modal.style.display = "block";
-            console.log("You entered wrong password");
-        }
-    })
-    if(userexist==false){
+    if(users==null){
         modalText.innerHTML="User not found.. Please signup.";
         modal.style.display = "block";
         console.log("User not found");
+    }
+    else{
+        users.forEach((element)=>{
+            if(username.value==element.user && pass.value==element.pass){
+                console.log("entered")
+                userexist=true;
+                window.location.href="home.html";
+            }
+            else if(username.value==element.user && pass.value!=element.pass){
+                userexist=true;
+                modalText.innerHTML="Entered wrong password";
+                modal.style.display = "block";
+                console.log("You entered wrong password");
+            }
+        })
+        if(userexist==false){
+            modalText.innerHTML="User not found.. Please signup.";
+            modal.style.display = "block";
+            console.log("User not found");
+        }
     }
     username.value="";
     pass.value="";
